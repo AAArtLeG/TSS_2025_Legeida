82ac4f55f21912c7fc63437f34fbceeffda43033
AAArtLeG <art.legeida@gmail.com>
2025-11-05 10:26:35 +0100

    Create openfolder slot and scanFolder function, that grab and save absolute path and name of all images from folder. Closes #6

--- src/TSS_project.cpp
+++ src/TSS_project.cpp
@@ -3,6 +3,10 @@
 #include <QFileDialog>
 #include <QImage>
 #include <QPixmap>
+#include <QVector>
+#include <QMessageBox>
+#include <QDebug>
+#include <QDirIterator>
 
 TSS_project::TSS_project(QWidget *parent)
     : QMainWindow(parent),
@@ -15,10 +19,13 @@ TSS_project::TSS_project(QWidget *parent)
     scene = new QGraphicsScene(this);
     ui->graphicsView->setScene(scene);
     currentImage = nullptr;
+    dataBase = new QVector<DataStorage>;
 }
 
 TSS_project::~TSS_project()
-{}
+{
+    delete dataBase;
+}
 
 void TSS_project::on_actionOpen_triggered()
 {
@@ -52,4 +59,63 @@ void TSS_project::on_actionSaveAs_triggered()
     item->setTransformationMode(Qt::SmoothTransformation);
     scene->setSceneRect(item->boundingRect());
     ui->graphicsView->fitInView(item, Qt::KeepAspectRatio);
+}
+
+void TSS_project::scanFolderOnce(const QString& dirPath) {
+    Q_ASSERT(dataBase);
+    dataBase->clear();
+
+    // for one directory
+    /*QDir d(dirPath);
+    d.setFilter(QDir::Files | QDir::NoDotAndDotDot);
+    d.setNameFilters({ "*.png","*.jpg","*.jpeg","*.bmp","*.gif","*.tif","*.tiff","*.webp" });
+    d.setSorting(QDir::Name | QDir::IgnoreCase);
+
+    const QFileInfoList infos = d.entryInfoList();
+    dataBase->reserve(infos.size());
+
+    for (const QFileInfo& fi : infos) { //fi metadata from photos
+        DataStorage i;
+        i.setImgName(fi.fileName());
+        i.setImgPath(fi.absoluteFilePath());
+
+        dataBase->push_back(i);
+    }
+    */
+
+    QDirIterator it(
+        dirPath, // root folder
+        { "*.png","*.jpg","*.jpeg","*.bmp","*.gif","*.tif","*.tiff","*.webp"}, // types of files
+        QDir::Files | QDir::NoDotAndDotDot,
+        QDirIterator::Subdirectories              
+    );
+
+    while (it.hasNext()) {
+        const QString path = it.next();
+        const QFileInfo fi(path);
+
+        DataStorage i;
+        i.setImgName(fi.fileName());
+        i.setImgPath(fi.absoluteFilePath());
+
+        dataBase->push_back(i);
+    }
+}
+
+void TSS_project::on_actionOpen_folder_triggered() {
+    const QString dir = QFileDialog::getExistingDirectory(this, tr("Select Folder"));
+    if (dir.isEmpty()) return;
+    scanFolderOnce(dir);
+
+    if (!dataBase->isEmpty()) {
+        const QString& path = dataBase->at(0).getImgPath();    
+        QImage img(path);
+        if (!img.isNull()) {
+            scene->clear();
+            item = scene->addPixmap(QPixmap::fromImage(img));
+            item->setTransformationMode(Qt::SmoothTransformation);
+            scene->setSceneRect(item->boundingRect());
+            ui->graphicsView->fitInView(item, Qt::KeepAspectRatio);
+        }
+    }
 }
--- src/TSS_project.h
+++ src/TSS_project.h
@@ -1,10 +1,9 @@
 #pragma once
-
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets>
+#include <QVector>
 #include "ui_TSS_project.h"
-  
-
+#include "DataStorage.h"
 
 class TSS_project : public QMainWindow
 {
@@ -21,9 +20,12 @@ private:
     QGraphicsPixmapItem* item = nullptr;
     QImage* currentImage;
 
+    QVector<DataStorage>* dataBase = nullptr;
 
+    void scanFolderOnce(const QString& dirPath);
 private slots:
     //fileDialogFunctions
     void on_actionOpen_triggered();
     void on_actionSaveAs_triggered();
+    void on_actionOpen_folder_triggered();
 };
